7e1e94da41d468aa65bc1321d7e78215f0440e41
Александр Паршин <cashkacaca@gmail.com>
2022-11-02 23:48:15 +0300

    Делаю валидацию комнаты VS гости

--- js/validation-pristine.js
+++ js/validation-pristine.js
@@ -1,37 +1,52 @@
-// родитель форма
-const adForm = document.querySelector('.ad-form'); // form class="ad-form"
-const type = adForm.querySelector('#type'); // Тип жилья
-const price = adForm.querySelector('#price'); // Цена за ночь, руб.
-const formTime = adForm.querySelector('.ad-form__element--time'); // Время заезда и выезда
-const timeIn = adForm.querySelector('#timein'); // Время заезда
-const timeOut = adForm.querySelector('#timeout'); // Время выезда
+const ROOMS_VS_GUEST = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
+};
+
+
+const adForm = document.querySelector('.ad-form'); // родитель форма form class="ad-form"
 const roomNumber = adForm.querySelector('#room_number'); // Количество комнат
-
 const capacity = adForm.querySelector('#capacity'); // Количество мест
-const guestNumber = capacity.querySelectorAll('option'); // тип жилья потом переименовать housing-type
 
+// const type = adForm.querySelector('#type'); // Тип жилья
+// const price = adForm.querySelector('#price'); // Цена за ночь, руб.
+// const formTime = adForm.querySelector('.ad-form__element--time'); // Время заезда и выезда
+// const timeIn = adForm.querySelector('#timein'); // Время заезда
+// const timeOut = adForm.querySelector('#timeout'); // Время выезда
+// const guestNumber = capacity.querySelectorAll('option'); // тип жилья потом переименовать housing-type
 
-const prestine = new Pristine(adForm, {
-  classTo: 'ad-form__label', // Элемент, на который будут добавляться классы
-});
+// const NumberOfGuests = {
 
-/*
-const orderForm = document.querySelector('.form');
+// };
 
+// // родитель форма
+// const adForm = document.querySelector('.ad-form'); // form class="ad-form"
 
-const pristine = new Pristine(orderForm, {
-  classTo: 'form__item', // Элемент, на который будут добавляться классы
-  errorClass: 'form__item--invalid', // Класс, обозначающий невалидное поле
-  successClass: 'form__item--valid', // Класс, обозначающий валидное поле
-  errorTextParent: 'form__item', // Элемент, куда будет выводиться текст с ошибкой
-  errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
-  errorTextClass: 'form__error' // Класс для элемента с текстом ошибки
-});
+// Функция простой валидации
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element', // Элемент, на который будут добавляться классы !!! Внимательно смотри CSS куда вешается класс
+  errorClass: 'ad-form__element--invalid', // Класс, обозначающий невалидное поле
+  errorTextParent: 'ad-form__element', // Элемент, куда будет выводиться текст с ошибкой (если повесить на label то ломается вёрстка)
+}, true // чтобы Pristine валидировала форму по мере ввода
+);
 
-
-orderForm.addEventListener('submit', (evt) => {
+// простая проверка полей заголовок объявления и цена за ночь
+adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
 });
 
+/*
+1 комната — «для 1 гостя»;
+2 комнаты — «для 2 гостей» или «для 1 гостя»;
+3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;
+100 комнат — «не для гостей».
 */
+const validateCapacity = () => {
+  ROOMS_VS_GUEST[roomNumber.value].includes(capacity.value);
+  // console.log(roomNumber);
+};
+
+
