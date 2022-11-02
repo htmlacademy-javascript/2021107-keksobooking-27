22d5f92ae8e0fb020a91b00d2ba2559720dc1dde
Александр Паршин <cashkacaca@gmail.com>
2022-11-02 13:34:47 +0300

    Рефакторинг функций в form.js

--- js/form.js
+++ js/form.js
@@ -1,41 +1,25 @@
 // родитель форма
-const formAds = document.querySelector('.ad-form');
+const formAd = document.querySelector('.ad-form');
 const mapFilter = document.querySelector('.map__filters');
+const disabledFields = document.querySelectorAll('fieldset, select.map__filter');
 
-
-//функция включения неактивного состояния у формы
-const disablingForm = (teg) => {
-  // Добавляем класс с окончанием--disabled к форме
-  teg.classList.add(`${teg.classList}--disabled`);
-
-  // Все интерактивные элементы формы должны быть заблокированы с помощью атрибута disabled
-  const fieldsets = teg.querySelectorAll('fieldset, select'); // выбираем теги fieldset и select
-  // перебираем forEach и каждому fieldset с помощью fieldset.disabled = true ставим атрибут disabled
-  fieldsets.forEach((fieldset) => {
-    // fieldset.setAttribute('disabled', 'disabled');
-    fieldset.disabled = true;
+// Функция добавления/удаления disabled у элементов с классами fieldset, select.map__filter
+const disablingEnablingForm = () => {
+  disabledFields.forEach((fieldItem) => {
+    // меняем состояние disabled на противоположное
+    fieldItem.disabled = !fieldItem.disabled;
   });
 };
 
-//функция выключения неактивного состояния у формы
-const enablingForm = (teg) => {
-  //  Удаляем 2 класс (с окончанием--disabled) у формы
-  teg.classList.remove(teg.classList[1]);
+// функция включения/выключения неактивного состояния у формы
+const disablingForm = () => {
+  // с помощью classList.toggle если нет/есть класс то добавляем/удаляем класс
+  formAd.classList.toggle('ad-form--disabled');
+  mapFilter.classList.toggle('map__filters--disabled');
 
-  // Все интерактивные элементы формы должны быть разблокированы с помощью удаления атрибута disabled
-  const fieldsets = teg.querySelectorAll('fieldset, select'); // выбираем теги fieldset и select
-  // перебираем forEach и каждому fieldset с помощью fieldset.disabled = false удаляем атрибут disabled
-  fieldsets.forEach((fieldset) => {
-    fieldset.disabled = false;
-  });
+  disablingEnablingForm();
 };
 
 
-// Выключение формы .ad-form
-disablingForm(formAds);
-// Выключение формы .map__filters
-disablingForm(mapFilter);
+disablingForm();
 
-// Включение формы
-enablingForm(formAds);
-enablingForm(mapFilter);
