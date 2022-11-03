d0328d2f8000110024d1e2ab1a67c5eabeae38a3
Александр Паршин <cashkacaca@gmail.com>
2022-11-03 15:16:26 +0300

    Выполнил задание: Правда или действие (часть 1)

--- js/validation-pristine.js
+++ js/validation-pristine.js
@@ -1,28 +1,25 @@
-const ROOMS_VS_GUEST = {
+import { numDecline } from './utils.js';
+
+const WORDS = ['комната', 'комнаты', 'комнат', 'гость', 'гостя', 'гостей']; // Слова для склонения
+
+const ROOMS_TO_GUEST = {
   1: ['1'],
   2: ['1', '2'],
   3: ['1', '2', '3'],
   100: ['0']
 };
 
+const GUESTS_IN_ROOM = {
+  0: ['100'],
+  1: ['1', '2', '3'],
+  2: ['1', '2'],
+  3: ['3']
+};
+
 
 const adForm = document.querySelector('.ad-form'); // родитель форма form class="ad-form"
 const roomNumber = adForm.querySelector('#room_number'); // Количество комнат
 const capacity = adForm.querySelector('#capacity'); // Количество мест
-
-// const type = adForm.querySelector('#type'); // Тип жилья
-// const price = adForm.querySelector('#price'); // Цена за ночь, руб.
-// const formTime = adForm.querySelector('.ad-form__element--time'); // Время заезда и выезда
-// const timeIn = adForm.querySelector('#timein'); // Время заезда
-// const timeOut = adForm.querySelector('#timeout'); // Время выезда
-// const guestNumber = capacity.querySelectorAll('option'); // тип жилья потом переименовать housing-type
-
-// const NumberOfGuests = {
-
-// };
-
-// // родитель форма
-// const adForm = document.querySelector('.ad-form'); // form class="ad-form"
 
 // Функция простой валидации
 const pristine = new Pristine(adForm, {
@@ -32,21 +29,44 @@ const pristine = new Pristine(adForm, {
 }, true // чтобы Pristine валидировала форму по мере ввода
 );
 
-// простая проверка полей заголовок объявления и цена за ночь
-adForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
+// проверяем массив[Количество комнат] совпадает Количество мест (если совпадение то true)
+const validateCapacity = () => ROOMS_TO_GUEST[roomNumber.value].includes(capacity.value);
 
-/*
-1 комната — «для 1 гостя»;
-2 комнаты — «для 2 гостей» или «для 1 гостя»;
-3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;
-100 комнат — «не для гостей».
-*/
-const validateCapacity = () => {
-  ROOMS_VS_GUEST[roomNumber.value].includes(capacity.value);
-  // console.log(roomNumber);
+// Сообщения о неправильной валидации
+const getCapacityErrorMessage = () => `Указанное количество комнат вмещает ${ROOMS_TO_GUEST[roomNumber.value].join(' или ')}  ${numDecline(+ROOMS_TO_GUEST[roomNumber.value], WORDS[3], WORDS[4], WORDS[5])}.`;
+const getRoomNumberErrorMessage = () => `Для указанного количества гостей требуется ${GUESTS_IN_ROOM[capacity.value].join(' или ')}  ${numDecline(+GUESTS_IN_ROOM[capacity.value], WORDS[0], WORDS[1], WORDS[2])}.`;
+
+// Функции: что нужно делать при изменении полей "Количество комнат" и "Количество мест"
+const onCapacityChange = () => {
+  pristine.validate(capacity); // валидация Количество мест; pristine.validate() метод возвращает булево значение: true, если форма валидна
+  pristine.validate(roomNumber); // валидация Количество комнат
+};
+
+const onRoomNumberChange = () => {
+  pristine.validate(capacity);
+  pristine.validate(roomNumber);
 };
 
 
+// Кастомные функции проверки валидации
+pristine.addValidator(
+  capacity,
+  validateCapacity,
+  getCapacityErrorMessage
+);
+
+pristine.addValidator(
+  roomNumber,
+  validateCapacity,
+  getRoomNumberErrorMessage
+);
+
+// выбор пункта из списка
+capacity.addEventListener('change', onCapacityChange);
+roomNumber.addEventListener('change', onRoomNumberChange);
+
+// простая проверка полей заголовок объявления и цена за ночь
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault(); // отменяется нажатие кнопки
+  pristine.validate();
+});
