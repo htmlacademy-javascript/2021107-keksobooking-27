c256861f50faa7e6512fb9b8518d83c1f2627075
Keks <keks@htmlacademy.ru>
2022-11-03 15:18:22 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form.js
@@ -0,0 +1,27 @@
+// родитель форма
+const adForm = document.querySelector('.ad-form');
+const mapFilter = document.querySelector('.map__filters');
+const disabledFields = document.querySelectorAll('fieldset, select.map__filter');
+
+// Функция добавления/удаления disabled у элементов с классами fieldset, select.map__filter
+const disablingEnablingForm = () => {
+  disabledFields.forEach((fieldItem) => {
+    // меняем состояние disabled на противоположное
+    fieldItem.disabled = !fieldItem.disabled;
+  });
+};
+
+// функция включения/выключения неактивного состояния у формы
+const disablingForm = () => {
+  // с помощью classList.toggle если нет/есть класс то добавляем/удаляем класс
+  adForm.classList.toggle('ad-form--disabled');
+  mapFilter.classList.toggle('map__filters--disabled');
+
+  disablingEnablingForm();
+};
+
+
+export {
+  disablingForm, // включения/выключения формы
+};
+
