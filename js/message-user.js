d4d0159a1176820ec6d92418eb4f24352e101c2a
Александр Паршин <cashkacaca@gmail.com>
2022-11-11 15:23:14 +0300

    Удалось удалить сообщение с помощью ESC

--- js/message-user.js
+++ js/message-user.js
@@ -4,27 +4,47 @@ import { isEscapeKey } from './utils.js';
 const patternSuccess = document.querySelector('#success').content.querySelector('.success');
 const body = document.querySelector('body');
 
+
+// // const onMessageEscKeydown = (evt) => {
+// //   if (isEscapeKey(evt)) {
+// //     evt.preventDefault();
+// //     // closeOpenMessage();
+// //     console.log('1111111111');
+// //   }
+// // };
+// // document.addEventListener('keydown', onMessageEscKeydown, {once:true});
+
 const onMessageEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
-    // evt.preventDefault();
-    closeOpenMessage();
-    // console.log('1111111111');
+    evt.preventDefault();
+    // closeOpenMessage();
+    console.log('1111111111');
+    body.removeChild(body.querySelector('.success'));
+
   }
 };
+document.addEventListener('keydown', onMessageEscKeydown,);
 
-function closeOpenMessage () {
-  body.removeChild('div.success');
-  document.addEventListener('keydown', onMessageEscKeydown, {once:true});
-}
 
+// function closeOpenMessage() {
+//   document.addEventListener('keydown', onMessageEscKeydown, { once: true });
+//   // body.querySelector('.success').remove();
+// }
+
+
+// // let modal = document.querySelector('.success');
+// // console.log(modal);
+
+
+//
 const getSuccessfulDownloadForm = () => {
   // клонирую patternSuccess со всем содержимым
   const ticetElement = patternSuccess.cloneNode(true);
 
   body.append(ticetElement);
-  return body;
 };
 
 export {
   getSuccessfulDownloadForm, // удачная загрузка
 };
+
